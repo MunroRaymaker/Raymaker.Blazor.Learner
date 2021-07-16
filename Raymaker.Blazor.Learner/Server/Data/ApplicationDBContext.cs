91a9648146b6e5cf2bf41c0fabf1056342f2b7ce
Jens Rud Nielsen <jenrud@gmail.com>
2021-07-16 11:09:22 +0200

    Add project files.

---
+++ Raymaker.Blazor.Learner/Server/Data/ApplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Raymaker.Blazor.Learner.Shared.Models;
+
+namespace Raymaker.Blazor.Learner.Server.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Developer> Developers { get; set; }
+    }
+}
